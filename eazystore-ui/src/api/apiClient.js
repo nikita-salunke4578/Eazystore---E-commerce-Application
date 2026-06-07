74b38bdf148f6370ac704f7cac91fed975324854
Nikita Salunke <nikitasalunke960@gmail.com>
2026-06-07 16:38:46 +0530

    updated api.js file

--- eazystore-ui/src/api/apiClient.js
+++ eazystore-ui/src/api/apiClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const apiClient = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}api/v1`,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
